253f8e85e55c6c74057e9aff84b589efe6bca17c
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-06-21 12:15:12 +0530

    app built using parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 {
   /* <div id="parent">
   <div id="child">
@@ -14,8 +17,8 @@ const nested = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
+    React.createElement("h1", { key: "h1" }, "I am a h1 tag"),
+    React.createElement("h2", { key: "h2" }, "I am a h2 tag"),
   ])
 );
 
