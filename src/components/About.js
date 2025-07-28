347c3c4110d65a50baab1cdffa26cdf201e17f33
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-07-28 21:55:45 +0530

    Basic Routes

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+      <h2>This application is all about Online Food Delivery</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us Page</h1>
+      <p>For any query mail to hungerhub.helpdesk@gmail.com</p>
+    </div>
+  );
+};
+
+export default Contact;
