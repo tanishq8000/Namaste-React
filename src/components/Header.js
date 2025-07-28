dc0b31aaa7dd57b8c5c291784298fe5a932307ff
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-07-28 22:35:48 +0530

    child routes and Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
+import { Link } from "react-router-dom";
 const Header = () => {
   const [logBtn, setLogBtn] = useState("Login");
 
@@ -11,9 +12,15 @@ const Header = () => {
       <h1 className="app-name">HungerHub</h1>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <li>
             <button
