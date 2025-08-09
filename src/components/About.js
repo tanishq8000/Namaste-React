94af4dccbb5800b7874286115d95be50b71ecd5c
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-08-10 00:30:26 +0530

    User class based comp

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
       <h2>This application is all about Online Food Delivery</h2>
+      <UserClass
+        name={"Tanishq Khandelwal (class props)"}
+        location={"Bengaluru"}
+      />
     </div>
   );
 };
