4194a226aa3173c85b4bf8755d07480a0e1577cc
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-08-11 22:49:33 +0530

    state var in class comp

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,12 +4,26 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     //console.log(props);
+    this.state = {
+      count1: 0,
+    };
   }
 
   render() {
     const { name, location } = this.props;
+    const { count1 } = this.state;
     return (
       <div className="user-card">
+        <h1>Count1 : {count1}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count1: this.state.count1 + 1,
+            });
+          }}
+        >
+          Increase
+        </button>
         <h2>Name : {name}</h2>
         <h3>Location : {location}</h3>
         <h4>Web Application Developer</h4>
