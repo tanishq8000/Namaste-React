2425c0e5cf04e8acf05c6016f63bba27b3bce820
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-08-24 19:09:31 +0530

    Lazy Loading

---
+++ src/components/Grocery.js
@@ -0,0 +1,15 @@
+const Grocery = () => {
+  return (
+    <>
+      <h3>
+        This is a separate small app inside our Big app with lots of components
+      </h3>
+      <h4>
+        That's why we are creating separate js file or bundling for this. And we
+        are lazy loading this component
+      </h4>
+    </>
+  );
+};
+
+export default Grocery;
